41500486c61f9dc53517245b546c9e4a370dc3a3
Kreatorpro <Kreatorpro@HP>
2019-08-24 12:47:11 -0500

    init

---
+++ Desktop/angular cli/heros/e2e/src/app.e2e-spec.ts
@@ -0,0 +1,62 @@
+// import { AppPage } from './app.po';
+// import { browser, logging } from 'protractor';
+
+// describe('workspace-project App', () => {
+//   let page: AppPage;
+
+//   beforeEach(() => {
+//     page = new AppPage();
+//   });
+
+//   it('should display welcome message', () => {
+//     page.navigateTo();
+//     expect(page.getTitleText()).toEqual('heros app is running!');
+//   });
+
+//   afterEach(async () => {
+//     // Assert that there are no errors emitted from the browser
+//     const logs = await browser.manage().logs().get(logging.Type.BROWSER);
+//     expect(logs).not.toContain(jasmine.objectContaining({
+//       level: logging.Level.SEVERE,
+//     } as logging.Entry));
+//   });
+// });
+
+import { browser, element, by, ElementFinder, ElementArrayFinder } from 'protractor';
+import { promise } from 'selenium-webdriver';
+
+import { TourOfHeroesPage } from './app.po';
+
+describe('Tour of heroes Dashboard', () => {
+  let page: TourOfHeroesPage;
+
+  beforeEach(() => {
+    page = new TourOfHeroesPage();
+  });
+
+  it('should display top 4 heroes', () => {
+    page.navigateTo();
+    expect(page.getTop4Heroes()).toEqual(['Mr. Nice', 'Narco', 'Bombasto', 'Celeritas']);
+  });
+
+  it('should navigate to heroes', () => {
+    page.navigateToHeroes();
+    expect(page.getAllHeroes().count()).toBe(11);
+  });
+});
+
+describe('Tour of heroes, heroes page', () => {
+  let page: TourOfHeroesPage;
+
+  beforeEach(() => {
+    page = new TourOfHeroesPage();
+    page.navigateToHeroes();
+  });
+
+  it('should add a new hero', () => {
+    const currentHeroes = page.getAllHeroes().count();
+    page.enterNewHeroInInput('My new Hero');
+    expect(page.getAllHeroes().count()).toBe(currentHeroes.then(n => n + 1));
+  });
+
+});
